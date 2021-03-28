9db2d18b30e9264b72f4a299f3123a22ba301f0a
Nugroho Wahyu Saputra <11950115153@students.uin-suska.ac.id>
2021-03-28 12:53:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/yuuapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.yuuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tampilan_menu);
+    }
+
+    public void clickRS(View view) {
+        //gambar RS diclick
+
+        Intent i = new Intent(this,RS.class);
+        startActivity(i);
+    }
+}
